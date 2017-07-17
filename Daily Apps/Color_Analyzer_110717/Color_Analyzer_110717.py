cc9edf74b07b52a57ff85d6f313bb7b3ef6ea802
Tyler Main <shalune@users.noreply.github.com>
2017-07-17 01:06:24 -0400

    finished color analyzer
    accepts standard comma separated RGB or Hex values

--- Daily Apps/Color_Analyzer_110717/Color_Analyzer_110717.py
+++ Daily Apps/Color_Analyzer_110717/Color_Analyzer_110717.py
@@ -1,19 +1,38 @@
 from tkinter import *
 import math
+import numpy
 
 
 hexToDecimal = {'1' : '1', '2' : '2', '3' : '3', '4' : '4',
                 '5' : '5', '6' : '6', '7' : '7', '8' : '8',
-                '9' : '9', '0' : '0', 'A' : '11', 'B' : '12',
-                'C' : '13', 'D' : '14', 'E' : '15', 'F' : '16'}
+                '9' : '9', '0' : '0', 'A' : '10', 'B' : '11',
+                'C' : '12', 'D' : '13', 'E' : '14', 'F' : '15'}
 huePrefix = 'hue = '
 lightnessPrefix = 'lightness = '
 saturationPrefix = 'saturation = '
+colorNameVectorPairs = {'Red' : lambda: redVector(), 'Blue' : lambda: blueVector(), 'Green' : lambda: greenVector(),
+                        'Purple' : lambda: purpleVector(), 'Orange' : lambda: orangeVector(), 'Yellow' : lambda: yellowVector()}
+RGBscale = 255
+
+
+def main():
+    window = Tk()
+    colorEntry = Entry(window)
+    submitButton = Button(window, text="Analyze")
+    results = Label(window, text='Enter a color')
+    submitButton['command'] = lambda: analyze(colorEntry, results)
+
+    colorEntry.pack()
+    results.pack()
+    submitButton.pack()
+    window.mainloop()
+
 
 def analyze(colorEntry, results):
    input = colorEntry.get()
    color = getColor(input)
    analyzeColor(color, results)
+
 
 def getColor(input):
     if input.count(",") > 0:
@@ -30,8 +49,8 @@ def getHexColor(input):
     input.upper()
     input = convertHexToDecimal(input)
     result = []
-    for i in range(len(input) / 2):
-        result.append((int(input[i]) * 16) + int(input[i + 1]))
+    for i in range(len(input) // 2):
+        result.append((int(input[i*2]) * 16) + int(input[i*2 + 1]))
     return result
 
 
@@ -43,28 +62,49 @@ def convertHexToDecimal(hex):
 
 
 def analyzeColor(color, results):
-    output = analyzeHue(color) + "\n"
+    colorVec = colorToVector(color)
+    output = analyzeHue(color, colorVec) + "\n"
     output += analyzeLightness(color) + "\n"
-    output += analyzeSaturation(color)
+    output += analyzeSaturation(colorVec)
     results['text'] = output
 
 
-def analyzeSaturation(color):
-
-    return
+def analyzeSaturation(colorVec):
+    magnitude = (vectorMagnitude(colorVec) / RGBscale) * 100
+    return saturationPrefix + str(math.floor(magnitude)) + '%'
 
 def analyzeLightness(color):
     total = 0
     for c in color:
-        total += int(color[c])
-    return lightnessPrefix + (total/3)
+        total += int(c)
+    value = (total/3)/RGBscale*100
+    return lightnessPrefix + str(value) + '%'
 
 
-def analyzeHue(color):
-    return
+def analyzeHue(color, colorVec):
+    closestColor = ''
+    closestMatch = 0
+    for name, vec in colorNameVectorPairs.items():
+        #dot = numpy.dot(colorVec, vec())
+        dot = dotProduct(colorVec, vec())
+        if dot > closestMatch:
+            closestMatch = dot
+            closestColor = name
+    return huePrefix + closestColor
 
-def boxProduct(cVec1, cVec2, cVec3):
-    return
+def colorToVector(color):
+    result = [0,0]
+    result[0] = colorVectorX(color)
+    result[1] = colorVectorY(color)
+    return result
+
+
+def colorVectorX(color):
+    return (redVector()[0] * int(color[0])) + (greenVector()[0] * int(color[1])) + (blueVector()[0] * int(color[2]))
+
+
+def colorVectorY(color):
+    return (redVector()[1] * int(color[0])) + (greenVector()[1] * int(color[1])) + (blueVector()[1] * int(color[2]))
 
 
 def redVector():
@@ -72,7 +112,7 @@ def redVector():
 
 
 def blueVector():
-    ratio = math.tan(math.pi / 6)
+    ratio = math.tan(math.pi / 3)
     o = 1
     a = o * ratio
     unitVector = normalizeVector(a, o)
@@ -81,22 +121,37 @@ def blueVector():
 
 
 def greenVector():
-    ratio = math.tan(math.pi / 6)
+    ratio = math.tan(math.pi / 3)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    unitVector[0] *= -1
+    unitVector[1] *= -1
+    return unitVector
+
+
+def yellowVector():
+    ratio = math.tan(math.pi * 4 / 9)
     o = 1
     a = o * ratio
     unitVector = normalizeVector(a, o)
     unitVector[0] *= -1
     return unitVector
 
-
-def yellowVector():
-    return
-
 def orangeVector():
-    return
+    ratio = math.tan(math.pi * 2 / 9)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    unitVector[0] *= -1
+    return unitVector
 
 def purpleVector():
-    return
+    ratio = math.tan(math.pi / 3)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    return unitVector
 
 
 def normalizeVector(x, y):
@@ -104,14 +159,39 @@ def normalizeVector(x, y):
     return [x/magnitude, y/magnitude]
 
 
-window = Tk()
-colorEntry = Entry(window)
-submitButton = Button(window, text="Analyze")
-results = Label(window, text='Enter a color')
-submitButton['command'] = analyze(colorEntry, results)
+def dotProduct(vec1, vec2):
+    result = sum([vec1[i]*vec2[i] for i in range(len(vec1))])
+    return result
 
 
-colorEntry.pack()
-results.pack()
-submitButton.pack()
-window.mainloop()
+def vectorMagnitude(vec):
+    result = 0
+    for val in vec:
+        result += math.pow(val,2)
+    return math.sqrt(result)
+
+
+def subtractVector(vec1,vec2):
+    result = []
+    for i in range(len(vec1)):
+        result.append(vec1[i] - vec2[i])
+    return result
+
+
+def vecXConstant(vec,c):
+    result = []
+    for i in range(len(vec)):
+        result.append(vec[i] * c)
+    return result
+
+
+def laGrange(cVec1, cVec2, cVec3):
+    dot1 = dotProduct(cVec3, cVec2)
+    dot2 = dotProduct(cVec3, cVec1)
+    set1 = vecXConstant(cVec1,dot1)
+    set2 = vecXConstant(cVec2,dot2)
+    return subtractVector(set1, set2)
+
+
+if __name__ == "__main__":
+    main()
--- Daily Apps/testing.py
+++ Daily Apps/testing.py
@@ -1,8 +1,150 @@
 from lxml.html import fromstring
 import requests
+import math
+import numpy
 
-webAddress = 'https://www.washingtonpost.com/politics/state-department-spent-more-than-15000-for-rooms-at-new-trump-hotel-in-vancouver/2017/07/12/5eba5d0c-61bf-11e7-84a1-a26b75ad39fe_story.html?utm_term=.27750d03c07e'
+input = '255,255,255'
+hexInput = 'FF9100'
+hexToDecimal = {'1' : '1', '2' : '2', '3' : '3', '4' : '4',
+                '5' : '5', '6' : '6', '7' : '7', '8' : '8',
+                '9' : '9', '0' : '0', 'A' : '10', 'B' : '11',
+                'C' : '12', 'D' : '13', 'E' : '14', 'F' : '15'}
 
-title = fromstring(requests.get(webAddress).content).findtext('.//title')
+def getRGBColor(input):
+    input.replace(',','')
+    return input.split(',')
 
-print(title)
+def getColor(input):
+    if input.count(",") > 0:
+        print('RGB')
+    else:
+        print('HEX')
+
+def getHexColor(input):
+    input.upper()
+    input = convertHexToDecimal(input)
+    result = []
+    for i in range(len(input) // 2):
+        result.append((int(input[i*2]) * 16) + int(input[i*2 + 1]))
+    return result
+
+
+def convertHexToDecimal(hex):
+    result = []
+    for c in hex:
+        result.append(hexToDecimal[c])
+    return result
+
+def colorToVector(color):
+    result = [0,0]
+    result[0] = colorVectorX(color)
+    result[1] = colorVectorY(color)
+    return result
+
+def colorVectorX(color):
+    r = redVector()[0] * int(color[0])
+    g = greenVector()[0] * int(color[1])
+    b = blueVector()[0] * int(color[2])
+    print('x = ' + str(r) + '   ' + str(g) + '   ' + str(b))
+    return r + g + b
+
+def colorVectorY(color):
+    r = redVector()[1] * int(color[0])
+    g = greenVector()[1] * int(color[1])
+    b = blueVector()[1] * int(color[2])
+    print('y = ' + str(r) + '   ' + str(g) + '   ' + str(b))
+    return r + g + b
+
+
+def redVector():
+    return [0,1]
+
+
+def blueVector():
+    ratio = math.tan(math.pi / 3)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    unitVector[1] *= -1
+    return unitVector
+
+
+def greenVector():
+    ratio = math.tan(math.pi / 3)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    unitVector[0] *= -1
+    unitVector[1] *= -1
+    return unitVector
+
+
+def yellowVector():
+    return [-1,0]
+
+def orangeVector():
+    ratio = math.tan(math.pi / 3)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    unitVector[0] *= -1
+    return unitVector
+
+def purpleVector():
+    ratio = math.tan(math.pi / 3)
+    o = 1
+    a = o * ratio
+    unitVector = normalizeVector(a, o)
+    return unitVector
+
+
+def normalizeVector(x, y):
+    magnitude = math.sqrt(math.pow(x,2) + math.pow(y,2))
+    return [x/magnitude, y/magnitude]
+
+def vectorMagnitude(vec):
+    result = 0
+    for val in vec:
+        result += math.pow(val,2)
+    return math.sqrt(result)
+
+def subtractVector(vec1,vec2):
+    result = []
+    for i in range(len(vec1)):
+        result.append(vec1[i] - vec2[i])
+    return result
+
+
+def vecXConstant(vec,c):
+    result = []
+    for i in range(len(vec)):
+        result.append(vec[i] * c)
+    return result
+
+
+def laGrange(cVec1, cVec2, cVec3):
+    dot1 = dotProduct(cVec3, cVec2)
+    dot2 = dotProduct(cVec3, cVec1)
+    set1 = vecXConstant(cVec1,dot1)
+    set2 = vecXConstant(cVec2,dot2)
+    return subtractVector(set1, set2)
+
+def dotProduct(vec1, vec2):
+    result = sum([vec1[i]*vec2[i] for i in range(len(vec1))])
+    return result
+
+#def boxProduct(vec1, vec2, vec3):
+#    cross = numpy.cross(vec2,vec3)
+#    return dotProduct(vec1, cross)
+
+color = getRGBColor(input)
+print(redVector())
+print(greenVector())
+print(blueVector())
+#print(int(color[0]))
+#print(int(color[1]))
+#print(int(color[2]))
+print(colorToVector(input))
+
+print(laGrange(redVector(),blueVector(),greenVector()))
+print(getHexColor(hexInput))
