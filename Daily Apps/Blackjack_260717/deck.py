8445c8064599c5ba072b2dbd1d6e8067e980984a
Tyler Main <shalune@users.noreply.github.com>
2017-07-26 21:31:57 -0400

    deck file was excluded from last commit ?

--- Daily Apps/Blackjack_260717/deck.py
+++ Daily Apps/Blackjack_260717/deck.py
@@ -3,18 +3,38 @@ import random
 
 
 class Deck:
-    cards = {}
-    drawnCards = {}
+    cards = []
+    drawnCards = []
+    numPacks = 0
 
 
-    def __init__(self, numPacks):
-        for i in range(1,numPacks):
+    def __init__(self, numPacks = 1):
+        self.numPacks = numPacks
+        self.createPacks()
+
+
+    def createPacks(self, numPacks = 0):
+        if numPacks == 0:
+            numPacks = self.numPacks
+        for i in range(1, numPacks):
             self.createOnePack()
-
 
     def createOnePack(self):
         for suit in cards.cardsuits:
             for type in cards.cardvalues:
-                cards.add(cards.Card(suit, type))
+                self.cards.add(cards.Card(suit, type))
             for type in range(2,10):
-                cards.add(cards.Card(suit, type))
+                self.cards.add(cards.Card(suit, type))
+
+
+    def drawCard(self):
+        drawnCard = random.choice(self.cards)
+        self.cards.remove(drawnCard)
+        self.drawnCards.add(drawnCard)
+        return drawnCard
+
+
+    def checkReset(self):
+        if len(cards) == 0:
+            self.cards = self.drawnCards
+            self.drawnCards = []
