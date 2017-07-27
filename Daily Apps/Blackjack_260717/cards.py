ac166ff9fa0518aefa74962bbbf026367f751626
Tyler Main <shalune@users.noreply.github.com>
2017-07-26 20:13:28 -0400

    defined geter Card.value()

--- Daily Apps/Blackjack_260717/cards.py
+++ Daily Apps/Blackjack_260717/cards.py
@@ -1,11 +1,22 @@
 
 cardsuits = {"c" : "clubs", "d" : "diamonds", "h" : "hearts", "s" : "spades"}
-cardtypes = {"ace" : 1, "ace" : 11, "jack" : 10, "queen" : 10, "king" : 10}
+cardvalues = {"ace" : 1, "ace" : 11, "jack" : 10, "queen" : 10, "king" : 10}
 
 class Card:
     suit = None
     type = None
 
+
     def __init__(self, suit, type):
+        #validate input
         self.suit = suit
         self.type = type
+
+
+
+
+
+    def value(self):
+        if type in cardvalues:
+            return cardvalues[type]
+        return int(type)
