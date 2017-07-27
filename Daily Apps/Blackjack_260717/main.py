066c680d1687492f6cf5d711c6d98519847a7dca
Tyler Main <shalune@users.noreply.github.com>
2017-07-26 21:34:29 -0400

    defined main.main() + main.checkPlayAgain()
    main.main() - starts game loop, calls stub for play fn
    
    main.checkPlayAgain() - prompts player to play again after hand complete

--- Daily Apps/Blackjack_260717/main.py
+++ Daily Apps/Blackjack_260717/main.py
@@ -1,7 +1,23 @@
 from Blackjack_260717 import cards
 
+yesno = {"y" : True, "n" : False}
 
 def main():
+    while(True):
+        playOneHand()
+        if not checkPlayAgain():
+            return
+
+def checkPlayAgain():
+    while(True):
+        selection = str.lower(input("Would you like you play another hand? (y/n)\n"))
+        if selection in yesno:
+            return yesno[selection]
+        else:
+            print("That's not a valid option, enter y or n \n\n")
+
+
+def playOneHand():
     return
 
 
