fac0d74f131fd2a39cc3ea4b5b71d8f5f7442cb3
Tyler Main <shalune@users.noreply.github.com>
2017-07-26 20:54:35 -0400

    implemented test for Card.validateSuit(suit)

--- Daily Apps/Blackjack_260717/mytests.py
+++ Daily Apps/Blackjack_260717/mytests.py
@@ -2,6 +2,8 @@ from Blackjack_260717 import cards
 
 def main():
     tValidateTypes()
+    print("\n")
+    tValidateSuits()
 
 
 def tValidateTypes():
@@ -9,11 +11,23 @@ def tValidateTypes():
     types = {"ace", "jack", "queen", "king"}
     c = cards.Card()
 
-    for i in range(1,15):
+    for i in range(-1,15):
         types.add(str(i))
 
     for tType in types:
         myTest(baseDescription + str(tType), c.validateType(tType))
+
+
+def tValidateSuits():
+    baseDescription = "valid card suit, checked suit: "
+    suits = {"c", "d", "h", "s", "q", "clown car"}
+    c = cards.Card()
+
+    for i in range(-2,2):
+        suits.add(str(i))
+
+    for tSuit in suits:
+        myTest(baseDescription + str(tSuit), c.validateSuit(tSuit))
 
 
 def myTest(description, conditional):
